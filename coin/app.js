e8920ead828ce5e7a24d693128c7b29a28d1c34f
younggil <zelord.kwoun@gmail.com>
2021-03-18 10:20:05 +0900

    Create app.js

---
+++ coin/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile); // 추가
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
